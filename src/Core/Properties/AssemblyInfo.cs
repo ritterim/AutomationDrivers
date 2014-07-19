c8bedd258e957668bdd77f9bebb42c4caeb6565d
JoshuaWiens <joshua.wiens@imt.bz>
2014-07-19 12:34:29 -0400

    Updated Assembly and Nuget versions

--- src/Core/Properties/AssemblyInfo.cs
+++ src/Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Core")]
+[assembly: AssemblyTitle("AutomationDrivers.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Core")]
+[assembly: AssemblyCompany("Insurance Marketing Technology")]
+[assembly: AssemblyProduct("AutomationDrivers.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- src/IisExpressHost/Properties/AssemblyInfo.cs
+++ src/IisExpressHost/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IisExpressHost")]
+[assembly: AssemblyTitle("AutomationDrivers.IisExpressHost")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IisExpressHost")]
+[assembly: AssemblyCompany("Insurance Marketing Technology")]
+[assembly: AssemblyProduct("AutomationDrivers.IisExpressHost")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
